83c98d72ab0828c5614158ed1142b393f00cb221
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-07 20:31:24 +0530

    Find the n th to Last Character in a String

---
+++ j35.js
@@ -0,0 +1,6 @@
+// Setup
+//Find the n th to Last Character in a String
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length-2]; // Change this line
