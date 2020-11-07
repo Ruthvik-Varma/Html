4aae3cb7e269d96576448920db99c98626310350
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-07 20:30:33 +0530

    Use Bracket Notation to Find the First Character in a String

---
+++ j31.js
@@ -0,0 +1,8 @@
+// Setup
+// Use Bracket Notation to Find the First Character in a String
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
