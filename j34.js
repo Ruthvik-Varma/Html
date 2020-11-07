d1629d4b26e71b71ae0ac12dc590a46926182249
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-07 20:31:12 +0530

    Find the Last Character in a String

---
+++ j34.js
@@ -0,0 +1,6 @@
+// Setup
+//Find the Last Character in a String
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
