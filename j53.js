a70757c723b5cd2722bcf23a388feb45b7bffe11
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:15:49 +0530

    Undefined Value returned from a Function

---
+++ j53.js
@@ -0,0 +1,18 @@
+// Setup
+//  Undefined Value returned from a Function
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+
+addThree();
+addFive();
