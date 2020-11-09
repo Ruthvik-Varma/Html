742626ba58b89046dcbba0ed7a57ae4b8d63369f
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 11:31:08 +0530

    Iterate with JavaScript While Loops

---
+++ j94.js
@@ -0,0 +1,13 @@
+// Setup
+//Iterate with JavaScript While Loops
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while(i<6 && i>=0){
+    myArray.push(i);
+    i--;
+    console.log(myArray);
+}
+
+console.log(myArray);
