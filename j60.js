cabb4aa6898ce6d0b7454b1a7acd0d2bed5df073
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:16:56 +0530

    comparing different values

---
+++ j60.js
@@ -0,0 +1,10 @@
+// Setup
+//comparing different values
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
