b62916f9fec4d753088e6a3ee42b46bc91902e3c
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:47:00 +0530

    Accessing Nested Objects

---
+++ j91.js
@@ -0,0 +1,17 @@
+// Setup
+// Accessing Nested Objects
+
+
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
