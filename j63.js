2e9360ea9fb35abefaf93bff344f0c81be6e5180
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:17:26 +0530

    the Greater Than Operator

---
+++ j63.js
@@ -0,0 +1,16 @@
+
+// the Greater Than Operator
+
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val >10) {  // Change this line
+    return "Over 10";
+  }
+  
+  return "10 or Under";
+}
+
+testGreaterThan(10);
