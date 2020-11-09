39fe01c9ab6a59b2c8f071c10116757b719b9a93
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:44:18 +0530

    Accessing Object Properties with Variables

---
+++ j84.js
@@ -0,0 +1,12 @@
+// Setup
+// Accessing Object Properties with Variables
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+
+var playerNumber = 16; // Change this Line
+var player = testObj[playerNumber]; // Change this Line
