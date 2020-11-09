3ed89095b132a7d2100ddb6a336e6a9876ae50bd
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:45:08 +0530

    Using Objects for Lookups

---
+++ j88.js
@@ -0,0 +1,23 @@
+// Setup
+//  Using Objects for Lookups
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+ var lookup = {
+    "alpha":"Adams",
+    "bravo":"Boston",
+    "charlie":"Chicago",
+    "delta":"Denver",
+    "echo":"Easy",
+    "foxtrot":"Frank"
+    };
+  
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+
+}
+
+phoneticLookup("charlie");
