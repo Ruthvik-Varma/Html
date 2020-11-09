6218d9d28973387b3a896388490e29b8e7bc9ce3
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:44:45 +0530

    Add New Properties

---
+++ j86.js
@@ -0,0 +1,12 @@
+// Setup
+// Add New Properties to a JavaScript Object
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+
+// Only change code below this line
