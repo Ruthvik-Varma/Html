be4a6d0703e4dc2db0cb766f59085d7f3471728c
Ruthvik-Varma <manohar.badarvada@gmail.com>
2020-11-09 09:43:04 +0530

    Build JavaScript Objects

---
+++ j81.js
@@ -0,0 +1,11 @@
+
+// Build JavaScript Objects
+var myDog = {
+// Only change code below this line
+  "name": "Bunny",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["me", "myslef"]
+
+// Only change code above this line
+};
